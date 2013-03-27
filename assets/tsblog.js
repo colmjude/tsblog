973da8c6206ec8815a9f5729a1248569517e58dc
Colm Britton <colmjude@gmail.com>
2013-03-27 14:08:22 +0000

    Connect up loadmore link so clicking loads next 10 posts

--- assets/tsblog.js
+++ assets/tsblog.js
@@ -36,16 +36,32 @@
 		});
 	}
 
+	function getPosts( current_index, callback ) {
+		var current_index = current_index || 0,
+			url = "tiddlers?select=tag:post;sort=-modified;limit=" + current_index + ",10;render=1";
+
+		$.getJSON( url, function(resp) {
+			if(resp.length) {
+				renderPosts( resp );
+				$(".loadmore").data("current-index", current_index + resp.length);
+			}
+		});
+	}
+
 	$(function() {
 		$content = $("#content");
 		posttmpl = Handlebars.compile(posttmplsource);
 
-		$.getJSON("tiddlers?select=tag:post;sort=-modified;limit=10;render=1", function(resp) {
-			if(resp.length) {
-				renderPosts( resp );
-			}
-		});
+		// get the initial set of posts
+		getPosts( 0, renderPosts );
 
+		// add handler to load more post on click
+		$(".loadmore").on("click", function(e) {
+			var $this = $(e.target),
+				current_index = $this.data("current-index");
+			getPosts( current_index, renderPosts );
+			return false;
+		});
 	});
 
 }(jQuery));
